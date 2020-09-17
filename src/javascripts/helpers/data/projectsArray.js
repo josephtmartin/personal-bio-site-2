9d01e526ec4284a3ff8501995bfc1167c09ebdf9
josephtmartin <goldglovejo13@gmail.com>
2020-09-17 15:34:44 -0500

    moving over from the first portfolio site

---
+++ src/javascripts/helpers/data/projectsArray.js
@@ -0,0 +1,73 @@
+const projects = [
+  {
+    title: 'Clothing Product Cards',
+    screenshot: 'https://i.postimg.cc/rpcYjQMQ/clothing-product-cards.png',
+    description:
+      "I built this as an exercise for my front end portion at NSS. The exercise was to build a grid of product cards for a company's products",
+    technologiesUsed: 'HTML, CSS, Version Control with Github',
+    available: false,
+    url: 'https://clothingproductcards.netlify.app/',
+    githubUrl: 'https://github.com/josephtmartin/ASSIGNMENT-product-cards',
+  },
+  {
+    title: 'Color Flipper',
+    screenshot: 'https://i.postimg.cc/SNrg4v28/color-flipper.png',
+    description:
+      'This project was built as a group exercise during my time at NSS',
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://joecolors.netlify.app/',
+    githubUrl:
+      'https://github.com/josephtmartin/color-flipper-hack-code-warriors-team-4',
+  },
+  {
+    title: 'Pet Adoption',
+    screenshot: 'https://i.postimg.cc/YSGkyXkB/pet-adoption.png',
+    description:
+      'This project filters through the pets and displays the type of pet based on which button is clicked. This was built during a group exercise',
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://petadoptioncenter.netlify.app/',
+    githubUrl:
+      'https://github.com/josephtmartin/color-flipper-hack-code-warriors-team-4',
+  },
+  {
+    title: 'Sorting Hat',
+    screenshot: 'https://i.postimg.cc/1tSmnGhx/sorting-hat.png',
+    description:
+      'This project was built as a group exercise during my time at NSS',
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://thesortinghatsite.netlify.app/',
+    githubUrl: 'https://github.com/josephtmartin/sorting-hat',
+  },
+  {
+    title: 'Bloomers Pants Store',
+    screenshot:
+      'https://pbs.twimg.com/profile_images/1097349761189859330/5652XPXc_400x400.jpg',
+    description: 'This is the best project',
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/josephtmartin',
+    githubUrl: 'https://github.com/josephtmartin',
+  },
+  {
+    title: 'Cool Project',
+    screenshot:
+      'https://pbs.twimg.com/profile_images/1097349761189859330/5652XPXc_400x400.jpg',
+    description: 'This is the best project',
+    technologiesUsed:
+      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    url: 'https://github.com/josephtmartin',
+    githubUrl: 'https://github.com/josephtmartin',
+  },
+];
+
+const getProjects = () => projects;
+
+export default { getProjects };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
+import display from './components/displayCards';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  display.displayProjects();
 };
 
 init();
