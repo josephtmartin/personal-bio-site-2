b3f91f85309fe095b6a1de014f7543e72064f78b
josephtmartin <goldglovejo13@gmail.com>
2020-09-22 15:34:30 -0500

    added the getProjects function

---
+++ src/javascripts/helpers/data/projectData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import apiKeys from '../../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getProjects = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/projects.json`).then((response) => {
+    const demProjects = response.data;
+    const projects = [];
+    if (demProjects) {
+      Object.keys(demProjects).forEach((projectId) => {
+        projects.push(projectId);
+      });
+    }
+    resolve(projects);
+  }).catch((error) => reject(error));
+});
+
+export default { getProjects };
